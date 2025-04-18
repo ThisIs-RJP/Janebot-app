e57e267360c9f651d18d0d200ec8adc613b8bbea
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2025-04-18 21:08:30 +0100

    feat(JB-29): write first blog post (#8)
    - added blog post box and made it reusable
    - added like button to the post

--- dev/src/components/desktop-view/MainPageDesktop.jsx
+++ dev/src/components/desktop-view/MainPageDesktop.jsx
@@ -10,6 +10,7 @@ import './../../styles/desktop-view/main-page-1.css';
 
 // Image imports
 import ChibiRobin from "./../../images/chibi_robin.jpeg"
+import RJIcon from "./../../images/ashleybop.gif"
 
 /* 
   ################## Main Configuration ################## 
@@ -17,6 +18,8 @@ import ChibiRobin from "./../../images/chibi_robin.jpeg"
 
 function MainPageDesktop() {
   const [darkMode, setDarkMode] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
 
   useEffect(() => {
     if (darkMode) {
@@ -28,6 +31,15 @@ function MainPageDesktop() {
 
   const toggleTheme = () => {
     setDarkMode(!darkMode);
+  };
+
+  const handleLikeClick = () => {
+    if (isLiked) {
+      setLikeCount(likeCount - 1);
+    } else {
+      setLikeCount(likeCount + 1);
+    }
+    setIsLiked(!isLiked);
   };
 
   return (
@@ -59,6 +71,67 @@ function MainPageDesktop() {
       <div className="full-page-divider" />
 
       <div className="right-box-container">
+        <div className="post-box-container" style={{ marginTop: "10vh"}}>
+          <div className="post-box">
+            <div className="inside-post-box">
+
+              {/* #### TOP BOX ### */}
+              <div className="post-box-top-box">
+
+                <div className="post-box-user-icon">
+                  <img src={RJIcon} />
+                </div>
+
+                <div className="post-box-user-icon-gap">
+                </div>
+
+                <div className="post-box-user-name-box">
+                  <div className="post-box-name-box-container">
+                    <div className="post-box-name-box-username">
+                      <div>
+                        Smoodeazy
+                      </div>
+                    </div>
+                    <div className="post-box-name-box-username-two">
+                      <div>
+                        @bestfullstackdeveloper
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* #### TOP BOX ### */}
+
+              {/* #### TEXT BOX #### */}
+              <div className="post-box-text-box">
+                First blog post! Can't wait to start developing this project!
+                <br />
+                <span className="hashtag">
+                  #janebot
+                </span>
+              </div>
+              {/* #### TEXT BOX #### */}
+
+              {/* #### TIME STAMP BOX #### */}
+              <div className="timestamp-container">
+                20:41PM 。 2023-04-18
+              </div>
+              {/* #### TIME STAMP BOX #### */}
+
+              {/* #### POST INTERACTIVE BOX #### */}
+              <button 
+                  className={`like-button ${isLiked ? 'liked' : ''}`}
+                  onClick={handleLikeClick}
+                  aria-label="Like post"
+              >
+                  ♥
+              </button>
+              <span className="like-count">{likeCount} {likeCount === 1 ? 'person likes' : 'people like'} this</span>
+              {/* #### POST INTERACTIVE BOX #### */}
+
+            </div>
+          </div>
+        </div> 
       </div>
     </div>
   )
